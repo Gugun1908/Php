f452b36868bc3d34dcfce0cceb284f3225faae15
Gugun1908 <136148277+Gugun1908@users.noreply.github.com>
2023-07-08 13:31:19 +0700

    Add files via upload
    Konversi sebuah nilai ke huruf

---
+++ konversi.php
@@ -0,0 +1,15 @@
+<?php 
+
+$nilai= 100;
+if ($nilai >= 85 and $nilai<=100){
+    echo "A";}
+else if($nilai >= 70 and $nilai<=84){
+    echo "B";}
+else if($nilai >= 60 and $nilai<70){
+    echo "C";}
+else if($nilai >= 50 and $nilai<60){
+     echo "D";}
+else if ($nilai<50){
+        echo "E";}
+
+?>
